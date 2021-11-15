7b4f1523beb797a55c5a28b294a1a129862d9b65
ducnguyennv <onguyn6@gmail.com>
2021-11-15 22:20:51 +0700

    Nguyen push code, have done 50%

--- src/DAO/DichVuDAO.java
+++ src/DAO/DichVuDAO.java
@@ -2,8 +2,61 @@
 
 
 package DAO;
+import Model.DichVu;
+import Utilities.XJdbc;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
-public class DichVuDAO {
+public class DichVuDAO extends QLRapPhimDAO<DichVu, String>{
+    private String insertSql = "INSERT INTO [DichVu]([MaDichVu],[TenDichVu],[GiaDichVu],[HIDE]) VALUES (?,?,?,?)";
+    private String updateSql = "UPDATE [DichVu] SET [TenDichVu] = ?, [GiaDichVu] = ?, [HIDE] = ? WHERE [MaDichVu] = ?";
+    private String deleteSql = "delete from dichvu where MaDichVu=?";
+    private String select_All_Sql = "Select * from dichvu";
+    private String select_sql_byID = "Select * from dichvu where MaDichVu=?";
+    
+    @Override
+    public void insert(DichVu enity) {
+        XJdbc.update(insertSql, enity.getMaDichVu(),enity.getTenDichVu(),enity.getGiaDichVu(),enity.getHide());
+    }
+
+    @Override
+    public void update(DichVu enity) {
+        XJdbc.update(updateSql, enity.getTenDichVu(),enity.getGiaDichVu(),enity.getHide(),enity.getMaDichVu());
+    }
+
+    @Override
+    public void delete(String key) {
+        XJdbc.update(deleteSql, key);
+    }
+
+    @Override
+    public List<DichVu> selectAll() {
+        return selectBySql(select_All_Sql);
+    }
+
+    @Override
+    public DichVu selectById(String key) {
+        return selectBySql(select_sql_byID, key).get(0);
+    }
+
+    @Override
+    protected List<DichVu> selectBySql(String sql, Object... args) {
+        List<DichVu> list = new ArrayList<>();
+        ResultSet rs = XJdbc.query(sql, args);
+        try {
+            while(rs.next()){
+                list.add(new DichVu(rs.getString("MaDichVu"),rs.getString("TenDichVu"),
+                                    rs.getFloat("GiaDichVu"),rs.getBoolean("Hide")));             
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DichVuDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
 
 }
--- src/DAO/KHTTDAO.java
+++ src/DAO/KHTTDAO.java
@@ -3,7 +3,62 @@
 
 package DAO;
 
+import Model.KHTT;
+import Utilities.XJdbc;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class KHTTDAO {
+
+public class KHTTDAO extends QLRapPhimDAO<KHTT, String>{
+    private String insertSql = "INSERT INTO [KhachHangThanThiet]([Ten],[SDT],[Email],[NgayDK],"
+                              + "[MucGiamGia],[SoLanSuDung],[MaNhanVien],[HIDE]) VALUES (?,?,?,?,?,?,?,?)";
+    private String updateSql = "UPDATE [KhachHangThanThiet] SET [Ten] = ?, [SDT] = ?, [Email] = ?, [NgayDK] = ?, "
+            + "                 [MucGiamGia] = ?, [SoLanSuDung] = ?, [MaNhanVien] = ?, [HIDE] = ? WHERE [MaKHTT] = ?";
+    private String deleteSql = "update [KhachHangThanThiet] set hide = 1 where [MaKHTT]=?";
+    private String select_All_Sql = "Select * from KhachHangThanThiet where hide = 0";
+    private String select_sql_byID = "Select * from KhachHangThanThiet where [MaKHTT]=? and where hide =0";
+    
+    @Override
+    public void insert(KHTT enity) {
+        XJdbc.update(insertSql, );
+    }
+
+    @Override
+    public void update(KHTT enity) {
+        XJdbc.update(updateSql, enity.getTenDichVu(),enity.getGiaDichVu(),enity.getHide(),enity.getMaDichVu());
+    }
+
+    @Override
+    public void delete(String key) {
+        XJdbc.update(deleteSql, key);
+    }
+
+    @Override
+    public List<KHTT> selectAll() {
+        return selectBySql(select_All_Sql);
+    }
+
+    @Override
+    public KHTT selectById(String key) {
+        return selectBySql(select_sql_byID, key).get(0);
+    }
+
+    @Override
+    protected List<KHTT> selectBySql(String sql, Object... args) {
+        List<KHTT> list = new ArrayList<>();
+        ResultSet rs = XJdbc.query(sql, args);
+        try {
+            while(rs.next()){
+                list.add();             
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DichVuDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
 
 }
